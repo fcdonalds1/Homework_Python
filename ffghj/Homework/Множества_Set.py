949e62e6c26d07931e8cf6953fd615d3fcd3a3d4
Vitali Herman <112175011+fcdonalds1@users.noreply.github.com>
2023-08-26 21:18:03 +0300

    Delete ffghj directory

--- ffghj/Homework/Множества_Set.py
+++
@@ -1,9 +0,0 @@
-first_set = {'Korn', 'Slipknot', 'Nirvana'}
-second_set = {'Iron', 'Maiden', 'Korn', 'Nirvana', 'Lee'}
-
-third_set = first_set.union(second_set)
-print(third_set)
-
-print(len(third_set))
-print(len(first_set))
-print(len(second_set))
--- ffghj/Практикум/Практика.py
+++
@@ -1,4 +0,0 @@
-
-
-for i in range(1 ,20000, 1000):
-    print(i ** 2)
