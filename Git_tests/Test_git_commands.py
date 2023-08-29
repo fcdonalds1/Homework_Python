9b47f4534bc3accd4cad90dcabebc164e2519049
Vitali Herman <axl_rose_90@mail.ru>
2023-08-30 00:06:48 +0300

    Logic operator_Python

--- Git_tests/Test_git_commands.py
+++ Git_tests/Test_git_commands.py
@@ -1,14 +1,3 @@
-password = '2233'
-a = input()
-count = 0
-while a != password:
-    print('Введите корректный пароль')
-    a = input()
-    count += 1
-else:
-    print('Пароль корректен')
-    print('Вы потратили,', count, 'попыток')
-
 
 
 
